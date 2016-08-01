8edc3c52af2b52c8af0480876d6ce72ebf66391f
dwSun <dwSun@Capsule>
2016-08-01 17:34:53 +0800

    version changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='doco',
-      version='1.2',
+      version='1.3',
       description='The visitors counter and weather...',
       author='dwSun',
       author_email='tonghuren@icloud.com',
