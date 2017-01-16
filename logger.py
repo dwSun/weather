9e611723ea854361d077ccb86eabd76ffa1487b7
dwSun <dwSun@Capsule>
2017-01-16 15:57:49 +0800

    add file log

--- logger.py
+++ logger.py
@@ -31,14 +31,24 @@ class Logger():
         self.logger = logging.getLogger('weather')
         self.logger.setLevel(logging.DEBUG)
 
+        # 创建一个handler，用于写入日志文件
+        fh = logging.handlers.RotatingFileHandler('backup.log',
+            maxBytes=2097152,
+            backupCount=10,
+        )
+
+        fh.setLevel(logging.DEBUG)
+
         # 再创建一个handler，用于输出到控制台
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
 
         # 定义handler的输出格式
+        fh.setFormatter(formatter)
         ch.setFormatter(formatter)
 
         # 给logger添加handler
+        self.logger.addHandler(fh)
         self.logger.addHandler(ch)
 
     def getlog(self):
