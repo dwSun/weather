acde87759ae8192239f69cad4e276c80431547f9
dwSun <dwSun@Capsule>
2017-01-16 15:36:02 +0800

    return multipile records.

--- record.py
+++ record.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from flask import Blueprint
+from flask import request
 from flask.ext.restful import Api, Resource
 from bson.objectid import ObjectId
 
@@ -25,10 +26,16 @@ class Record(Resource):
         if idx:
             log.debug('getting [{0}]'.format(idx))
             rc = db.weather.find_one({"_id": ObjectId(idx)})
+            rc['_id'] = str(rc['_id'])
         else:
-            rc = db.weather.find_one()
+            limit = 100
+            if request.args.get('limit'):
+                limit = int(request.args.get('limit').strip('/'))
+            rcs = db.weather.find({}, {"_id": 1}).limit(limit)
+            rc = []
+            for r in rcs:
+                rc.append(str(r['_id']))
 
-        rc['_id'] = str(rc['_id'])
         return {'code': 0,
                 'msg': '',
                 'payload': {
