22b435573cf6a0a850dc2c4b68f48cfd6c96d38f
dwSun <dwSun@Capsule>
2017-02-03 17:22:59 +0800

    parameter adjust

--- backup.py
+++ backup.py
@@ -9,19 +9,19 @@ from multiprocessing.dummy import Pool
 from db import db
 from logger import Logger
 
-l = Logger('backuo.log', log_f=True)
+l = Logger('backup.log', log_f=True)
 trace = l.trace
 log = l.getlog()
 
-limit = 10
-api = 'http://www.dwsun.com.cn//api/record/{0}/'
+limit = 100
+api = 'http://www.dwsun.com.cn/api/record/{0}/'
 
 apiLimit = api.format('?limit={0}'.format(limit))
 
 
 def retrieve(idx):
     try:
-        r = requests.get(api.format(idx), timeout=180)
+        r = requests.get(api.format(idx), timeout=60)
         ret = r.json()
         weather = ret['payload']['data']
         idx = weather['_id']
@@ -29,21 +29,21 @@ def retrieve(idx):
         weather['_id'] = ObjectId(idx)
 
         db.weather.save(weather)
-        r = requests.delete(api.format(idx), timeout=180)
+        r = requests.delete(api.format(idx), timeout=60)
     except requests.exceptions.ConnectionError as ex:
         log.error('failed with[{0}]'.format(ex))
 
 
 def main():
     while True:
-        r = requests.get(apiLimit, timeout=180)
+        r = requests.get(apiLimit, timeout=60)
         ret = r.json()
         weather = ret['payload']['data']
         count = ret['payload']['count']
-        if count < 1000:
+        if count < 10:
             log.debug('remote count[{0}], stop retrieve'.format(count))
             break
-        pool = Pool()
+        pool = Pool(32)
         pool.map(retrieve, weather)
         pool.close()
         pool.join()
